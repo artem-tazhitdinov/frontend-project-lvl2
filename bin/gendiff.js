4b15188323b3330f7f81a1ca72c48679516a319a
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-07-06 17:48:56 +0500

    Updating the help output and adding ESlint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
+import { Command } from 'commander';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
- 
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
+
 program.parse();
