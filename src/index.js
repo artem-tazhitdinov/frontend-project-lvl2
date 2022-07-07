2e93065f8055cd655533c273c4383c651555e2b8
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-07-07 10:45:58 +0500

    Added file reading functionality

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+import fs from 'fs';
+
+const genDiff = (filepath1, filepath2) => {
+  const currentPath = process.cwd();
+
+  const firstFileName = path.parse(filepath1).base;
+  const secondFileName = path.parse(filepath2).base;
+
+  const firstFilePath = path.resolve(currentPath, path.parse(filepath1).dir, firstFileName);
+  const secondFilePath = path.resolve(currentPath, path.parse(filepath2).dir, secondFileName);
+
+  const objectOne = JSON.parse(fs.readFileSync(firstFilePath, 'utf-8'));
+  const objectTwo = JSON.parse(fs.readFileSync(secondFilePath, 'utf-8'));
+
+  console.log(objectOne);
+  console.log(objectTwo);
+};
+
+export default genDiff;
