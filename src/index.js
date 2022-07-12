63557638325d23b989b0e004673af54dd5168eee
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-07-12 14:14:46 +0500

    Changed logic of the diff

--- src/index.js
+++ src/index.js
@@ -2,45 +2,39 @@ import _ from 'lodash';
 import { parseData } from './parsers.js';
 
 const findDifference = (obj1, obj2) => {
-  const firstObjectEntries = Object.entries(obj1);
-  const secondObjectEntries = Object.entries(obj2);
-  const allEntriesOfObjects = _.union(firstObjectEntries, secondObjectEntries).sort();
+  const obj1Keys = Object.keys(obj1);
+  const obj2keys = Object.keys(obj2);
+  const objectsKeys = _.union(obj1Keys, obj2keys).sort();
 
-  const result = allEntriesOfObjects.reduce((acc, [key, value]) => {
-    if (_.isObject(value)) {
-      return findDifference(obj1[key], obj2[key]);
-    }
-    if (!Object.hasOwn(obj2, key)) {
-      acc[`- ${key}`] = obj1[key];
-    } else if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      if (obj1[key] !== obj2[key]) {
-        acc[`- ${key}`] = obj1[key];
-        acc[`+ ${key}`] = obj2[key];
-      } else {
-        acc[`  ${key}`] = obj1[key];
-      }
+  const result = [];
+  for (const key of objectsKeys) {
+    if (!Object.hasOwn(obj1, key)) {
+      result.push({ key, value: obj2[key], status: 'added' });
+    } else if (!Object.hasOwn(obj2, key)) {
+      result.push({ key, value: obj1[key], status: 'deleted' });
+    } else if (obj1[key] !== obj2[key]) {
+      result.push({ key, value: obj2[key], oldValue: obj1[key], status: 'changed' });
     } else {
-      acc[`+ ${key}`] = obj2[key];
+      result.push({ key, value: obj1[key], status: 'unchanged' });
     }
-    return acc;
-  }, {});
+  }
   return result;
 };
-
+/*
 const convertToString = (obj) => {
   const stringifyObject = JSON.stringify(obj, null, 2);
   const stringifyStyledObject = stringifyObject.replaceAll(/"|,/g, '');
   return stringifyStyledObject;
 };
-
+*/
 const genDiff = (filepath1, filepath2) => {
   const objectOne = parseData(filepath1);
   const objectTwo = parseData(filepath2);
 
   const objectsDifference = findDifference(objectOne, objectTwo);
-  const differenceString = convertToString(objectsDifference);
-
-  return differenceString;
+  // const differenceString = convertToString(objectsDifference);
+  return objectsDifference;
+  // return differenceString;
 };
 
 export default genDiff;
