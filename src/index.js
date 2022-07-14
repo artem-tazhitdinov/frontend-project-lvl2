beb150dae27dfc0a73ab838253e78d2e17dbcc76
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-07-14 12:02:11 +0500

    Added Diff-builder

--- src/index.js
+++ src/index.js
@@ -4,39 +4,32 @@ import { parseData } from './parsers.js';
 const findDifference = (obj1, obj2) => {
   const obj1Keys = Object.keys(obj1);
   const obj2keys = Object.keys(obj2);
-  const objectsKeys = _.union(obj1Keys, obj2keys).sort();
+  const objectsKeys = _.sortBy(_.union(obj1Keys, obj2keys));
 
-  const result = objectsKeys.reduce((acc, key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      acc.push({ key, value: obj2[key], status: 'added' });
-    } else if (!Object.hasOwn(obj2, key)) {
-      acc.push({ key, value: obj1[key], status: 'deleted' });
-    } else if (obj1[key] !== obj2[key]) {
-      acc.push({
-        key, value: obj2[key], oldValue: obj1[key], status: 'changed',
-      });
-    } else {
-      acc.push({ key, value: obj1[key], status: 'unchanged' });
+  const diffResult = objectsKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return { key, value: obj2[key], status: 'added' };
     }
-    return acc;
-  }, []);
-  return result;
+    if (!_.has(obj2, key)) {
+      return { key, value: obj1[key], status: 'removed' };
+    }
+    if (obj1[key] === obj2[key]) {
+      return { key, value: obj1[key], status: 'unchanged' };
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, value: findDifference(obj1[key], obj2[key]), status: 'nested' };
+    }
+    return { key, value: { previousValue: obj1[key], newValue: obj2[key] }, status: 'updated' };
+  });
+  return diffResult;
 };
-/*
-const convertToString = (obj) => {
-  const stringifyObject = JSON.stringify(obj, null, 2);
-  const stringifyStyledObject = stringifyObject.replaceAll(/"|,/g, '');
-  return stringifyStyledObject;
-};
-*/
+
 const genDiff = (filepath1, filepath2) => {
   const objectOne = parseData(filepath1);
   const objectTwo = parseData(filepath2);
 
   const objectsDifference = findDifference(objectOne, objectTwo);
-  // const differenceString = convertToString(objectsDifference);
-  return JSON.stringify(objectsDifference, null, 2);
-  // return differenceString;
+  return objectsDifference;
 };
 
 export default genDiff;
