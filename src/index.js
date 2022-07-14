5e84b416c701e487b32b66f60b8b9b4b85bbe805
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-07-14 08:08:14 +0500

    Code Refactoring

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 import _ from 'lodash';
 import { parseData } from './parsers.js';
 
-const findDifference = (object1, object2) => {
-  const iter = (obj1, obj2) => {
-    const obj1Keys = Object.keys(obj1);
-    const obj2keys = Object.keys(obj2);
-    const objectsKeys = _.union(obj1Keys, obj2keys).sort();
+const findDifference = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1);
+  const obj2keys = Object.keys(obj2);
+  const objectsKeys = _.union(obj1Keys, obj2keys).sort();
 
-    const result = objectsKeys.reduce((acc, key) => {
-      if (!Object.hasOwn(obj1, key)) {
-        acc.push({ key, value: obj2[key], status: 'added' });
-      } else if (!Object.hasOwn(obj2, key)) {
-        acc.push({ key, value: obj1[key], status: 'deleted' });
-      } else if (obj1[key] !== obj2[key]) {
-        acc.push({
-          key, newValue: obj2[key], oldValue: obj1[key], status: 'changed',
-        });
-      } else {
-        acc.push({ key, value: obj1[key], status: 'unchanged' });
-      }
-      return acc;
-    }, []);
-
-    return [...result];
-  };
-  return iter(object1, object2);
+  const result = objectsKeys.reduce((acc, key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      acc.push({ key, value: obj2[key], status: 'added' });
+    } else if (!Object.hasOwn(obj2, key)) {
+      acc.push({ key, value: obj1[key], status: 'deleted' });
+    } else if (obj1[key] !== obj2[key]) {
+      acc.push({
+        key, value: obj2[key], oldValue: obj1[key], status: 'changed',
+      });
+    } else {
+      acc.push({ key, value: obj1[key], status: 'unchanged' });
+    }
+    return acc;
+  }, []);
+  return result;
 };
 /*
 const convertToString = (obj) => {
@@ -39,7 +35,7 @@ const genDiff = (filepath1, filepath2) => {
 
   const objectsDifference = findDifference(objectOne, objectTwo);
   // const differenceString = convertToString(objectsDifference);
-  return objectsDifference;
+  return JSON.stringify(objectsDifference, null, 2);
   // return differenceString;
 };
 
