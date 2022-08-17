236c9a337a4dab4077050ff8dbebb88b657dcb08
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-08-17 15:19:26 +0500

    Removed vars from tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { test, expect, beforeEach } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
@@ -9,19 +9,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let jsonFile1;
-let jsonFile2;
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
 
-let ymlFile1;
-let ymlFile2;
-
-beforeEach(() => {
-  jsonFile1 = getFixturePath('file1.json');
-  jsonFile2 = getFixturePath('file2.json');
-
-  ymlFile1 = getFixturePath('file1.yml');
-  ymlFile2 = getFixturePath('file2.yml');
-});
+const ymlFile1 = getFixturePath('file1.yml');
+const ymlFile2 = getFixturePath('file2.yml');
 
 test('Testing stylish formatter work with JSON files', async () => {
   const actual = genDiff(jsonFile1, jsonFile2, 'stylish');
