08cb670fd5f60e93eccfe1743b01e934f841998b
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-08-23 21:12:31 +0500

    Fixed default export

--- src/parsers.js
+++ src/parsers.js
@@ -1,6 +1,8 @@
 import yaml from 'js-yaml';
 
-export const parseData = (data, extension) => {
+const parseData = (data, extension) => {
   const parserExtensions = { json: JSON.parse, yml: yaml.load };
   return parserExtensions[extension](data);
 };
+
+export default parseData;
