7c0a16995b3d70b77249bb5898a5a3c16f036931
Tazhitdinov Artem <tazhitdinovartem@yandex.ru>
2022-08-29 14:03:13 +0500

    Code refactoring

--- src/buildTree.js
+++ src/buildTree.js
@@ -9,13 +9,13 @@ const buildTree = (data1, data2) => {
     if (!_.has(data2, key)) {
       return { key, value: data1[key], type: 'removed' };
     }
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return { key, value: buildTree(data1[key], data2[key]), type: 'complex' };
     }
-    if (data1[key] !== data2[key]) {
-      return { key, value: { value1: data1[key], value2: data2[key] }, type: 'updated' };
+    if (_.isEqual(data1[key], data2[key])) {
+      return { key, value: data1[key], type: 'unchanged' };
     }
-    return { key, value: data1[key], type: 'unchanged' };
+    return { key, value: { value1: data1[key], value2: data2[key] }, type: 'updated' };
   });
 };
 
